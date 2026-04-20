e58e129b4a30535c65902019a6a953f0defe5f83
James Kiarie <jamesmbugua349@gmail.com>
2026-04-21 02:40:12 +0300

    Push the details on cores headers

--- config/settings.py
+++ config/settings.py
@@ -215,12 +215,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://fahari.royalsoftwares.co.ke"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https//api.royalsoftwares.co.ke"
 ]
 
 
